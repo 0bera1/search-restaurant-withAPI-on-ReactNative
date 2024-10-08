6fdebdc31bd97cfc3bfca5dee41043007951ecab
Bera <102685755+0bera1@users.noreply.github.com>
2024-10-08 05:15:21 +0300

    Update yelp.js

--- api/yelp.js
+++ api/yelp.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer LKKcG95qXklU85GLtaYrpGmEAkqTInOde4TN588iEP30xsLbPBAX9HUtgVbLr5j5_lm8DO_cqK3hNtV4HMTDrzptZDTlmVVymTTeRUYcV5RA_r1-wd32VDzpIRDJZnYx',
+        Authorization: 'YOUR_API_KEY',
     },
 });
